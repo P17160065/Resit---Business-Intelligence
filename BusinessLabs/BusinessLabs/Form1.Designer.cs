d5a111dbeaf2266f2efcf38753629377c45d0795
p17160065 <p17160065@GWHXX652WIN005W>
2019-03-05 13:48:17 +0000

    week 4

--- BusinessLabs/BusinessLabs/Form1.Designer.cs
+++ BusinessLabs/BusinessLabs/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonGetDates = new System.Windows.Forms.Button();
+            this.listBoxDates = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonGetDates
+            // 
+            this.buttonGetDates.Location = new System.Drawing.Point(49, 167);
+            this.buttonGetDates.Name = "buttonGetDates";
+            this.buttonGetDates.Size = new System.Drawing.Size(120, 23);
+            this.buttonGetDates.TabIndex = 0;
+            this.buttonGetDates.Text = "Get Dates";
+            this.buttonGetDates.UseVisualStyleBackColor = true;
+            this.buttonGetDates.Click += new System.EventHandler(this.buttonGetDates_Click);
+            // 
+            // listBoxDates
+            // 
+            this.listBoxDates.FormattingEnabled = true;
+            this.listBoxDates.Location = new System.Drawing.Point(49, 196);
+            this.listBoxDates.Name = "listBoxDates";
+            this.listBoxDates.Size = new System.Drawing.Size(120, 95);
+            this.listBoxDates.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(49, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(245, 329);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBoxDates);
+            this.Controls.Add(this.buttonGetDates);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonGetDates;
+        private System.Windows.Forms.ListBox listBoxDates;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- BusinessLabs/BusinessLabs/Form1.cs
+++ BusinessLabs/BusinessLabs/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,24 @@ namespace BusinessLabs
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonGetDates_Click(object sender, EventArgs e)
+        {
+            //create list
+            List<string> Dates = new List<string>();
+            //clear listbox to ensure old date is out
+            listBoxDates.Items.Clear();
+            //create database string
+            string connectionString = Properties.Settings.Default.Data_set_1_1_ConnectionString;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand getDates = new OleDbCommand("SELECT [Order Date], [Ship Date] from Sheet1", connection);
+
+            }
         }
     }
 }
